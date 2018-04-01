2be3d59f93604bcff9520819165f6f91d56c4dfa
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-04-01 13:36:39 +0530

    Implement PreferenceFragmentCompat

--- app/src/main/java/com/ivantha/mobileatm/fragment/SettingsFragment.java
+++ app/src/main/java/com/ivantha/mobileatm/fragment/SettingsFragment.java
@@ -1,18 +1,11 @@
 package com.ivantha.mobileatm.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.ivantha.mobileatm.R;
 
-public class SettingsFragment extends Fragment {
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
@@ -20,13 +13,9 @@ public class SettingsFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreatePreferences(Bundle bundle, String s) {
+        // Load the Preferences from the XML file
+        addPreferencesFromResource(R.xml.settings);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
-    }
 }
