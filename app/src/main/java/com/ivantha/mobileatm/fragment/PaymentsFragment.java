59369ba0bd1c5953f1cf100dc88eb6959711d66c
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-04-19 16:30:22 +0530

    Complete Payments Interface

--- app/src/main/java/com/ivantha/mobileatm/fragment/PaymentsFragment.java
+++ app/src/main/java/com/ivantha/mobileatm/fragment/PaymentsFragment.java
@@ -5,29 +5,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.ivantha.mobileatm.R;
 
 public class PaymentsFragment extends Fragment {
 
+    private Spinner paymentsCategorySpinner;
+    private Spinner paymentsPayeeSpinner;
+
     public PaymentsFragment() {
         // Required empty public constructor
     }
 
-    public static RechargeFragment newInstance() {
-        RechargeFragment fragment = new RechargeFragment();
+    public static PaymentsFragment newInstance() {
+        PaymentsFragment fragment = new PaymentsFragment();
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payments, container, false);
+        View view = inflater.inflate(R.layout.fragment_payments, container, false);
+
+        // Payment Categories
+        paymentsCategorySpinner = view.findViewById(R.id.paymentsCategorySpinner);
+        ArrayAdapter<CharSequence> paymentsCategoryAdapter = ArrayAdapter.createFromResource(
+                view.getContext(),
+                R.array.payments_categories,
+                android.R.layout.simple_spinner_item);
+        paymentsCategoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paymentsCategorySpinner.setAdapter(paymentsCategoryAdapter);
+
+        // Payment Payees
+        paymentsPayeeSpinner = view.findViewById(R.id.paymentsPayeeSpinner);
+        ArrayAdapter<CharSequence> paymentsPayeeAdapter = ArrayAdapter.createFromResource(
+                view.getContext(),
+                R.array.payment_payees,
+                android.R.layout.simple_spinner_item);
+        paymentsPayeeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paymentsPayeeSpinner.setAdapter(paymentsPayeeAdapter);
+
+
+        return view;
     }
 
 }
