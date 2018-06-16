9e145ae3afd1c357c9bc7b036f7da60080c573da
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-06-16 09:58:25 +0530

    Upgrade dependancies

--- app/src/main/java/com/ivantha/mobileatm/common/Session.kt
+++ app/src/main/java/com/ivantha/mobileatm/common/Session.kt
@@ -48,27 +48,27 @@ object Session {
                 .create()
 
         // Initialize deals
-        db.child("deals").addChildEventListener(object : ChildEventListener {
-            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                deals[dataSnapshot.key] = dataSnapshot.getValue<Deal>(Deal::class.java)!!
-            }
-
-            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String) {
-                deals[dataSnapshot.key] = dataSnapshot.getValue<Deal>(Deal::class.java)!!
-            }
-
-            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
-                deals.remove(dataSnapshot.key)
-            }
-
-            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String) {
-                deals[dataSnapshot.key] = dataSnapshot.getValue<Deal>(Deal::class.java)!!
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                TODO("Failed to read value")
-            }
-        })
+//        db.child("deals").addChildEventListener(object : ChildEventListener {
+//            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+//                deals[dataSnapshot.key] = dataSnapshot.getValue<Deal>(Deal::class.java)!!
+//            }
+//
+//            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String) {
+//                deals[dataSnapshot.key] = dataSnapshot.getValue<Deal>(Deal::class.java)!!
+//            }
+//
+//            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+//                deals.remove(dataSnapshot.key)
+//            }
+//
+//            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String) {
+//                deals[dataSnapshot.key] = dataSnapshot.getValue<Deal>(Deal::class.java)!!
+//            }
+//
+//            override fun onCancelled(databaseError: DatabaseError) {
+//                TODO("Failed to read value")
+//            }
+//        })
 
         // Add deal colors
         dealColors.add(Color.parseColor("#e53935"))
