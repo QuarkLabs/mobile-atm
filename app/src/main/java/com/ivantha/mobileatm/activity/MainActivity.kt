35c2f009b4f6067d7c359d51f45f7e342ece53a8
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-06-18 03:58:54 +0530

    Implement transaction POST request

--- app/src/main/java/com/ivantha/mobileatm/activity/MainActivity.kt
+++ app/src/main/java/com/ivantha/mobileatm/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ivantha.mobileatm.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
@@ -10,13 +11,20 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.text.TextUtils
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.Toast
+import com.android.volley.*
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.gson.JsonParser
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
@@ -31,12 +39,20 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
-import org.jetbrains.annotations.Contract
+import org.bouncycastle.asn1.ua.DSTU4145NamedCurves.params
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
+    private val requestQueue: RequestQueue? = null
+        get() {
+            if (field == null) {
+                return Volley.newRequestQueue(applicationContext)
+            }
+            return field
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,7 +99,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             intentIntegrator.setBeepEnabled(true)
             intentIntegrator.setBarcodeImageEnabled(true)
             intentIntegrator.initiateScan()
-
         }
 
         val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -91,8 +106,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         navigationView.setNavigationItemSelectedListener(this)
-
-        mainActivity = this@MainActivity
 
         // Set Home as default view
         val manager = supportFragmentManager
@@ -103,6 +116,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         // Set Picasso debugging
         Picasso.with(baseContext).setIndicatorsEnabled(true)
+
+        mainActivity = this@MainActivity
     }
 
     override fun onBackPressed() {
@@ -119,10 +134,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    /**
+     * Handle action bar item clicks here.
+     * The action bar will automatically handle clicks on the Home/Up button,
+     * so long as you specify a parent activity in AndroidManifest.xml.
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
 
         if (id == R.id.action_help) {
@@ -166,30 +183,41 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
-            if (result.contents == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
-            } else {
+            if (result.contents != null) {
                 Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
 
-                var transaction: Transaction = TransactionServices.transactionFromJson(result.contents)
-                when (transaction.intention) {
-                    Transaction.Intention.SEND -> {
-                        // Receive cash sent by the other party
-                        receiveCash(transaction)
-                    }
-                    Transaction.Intention.REQUEST -> {
-                        // Send cash requested by the other party
-                        sendCash(transaction)
+                val transaction: Transaction = TransactionServices.transactionFromJson(result.contents)
+                transaction.receiverId = FirebaseAuth.getInstance().currentUser!!.uid
+
+                val dialogClickListener = DialogInterface.OnClickListener { dialogInterface: DialogInterface, i: Int ->
+                    when (i) {
+                        DialogInterface.BUTTON_POSITIVE -> {
+                            transaction.confirmed = true
+                            sendTransaction(transaction)
+                        }
+                        DialogInterface.BUTTON_NEGATIVE -> {
+                            transaction.confirmed = false
+                            sendTransaction(transaction)
+                        }
                     }
                 }
+
+                val builder = AlertDialog.Builder(this@MainActivity)
+                builder.setTitle("Do you confirm this payment?")
+                        .setMessage(transaction.getStatement())
+                        .setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener)
+                        .show()
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
-
-    private fun updateUI(user: FirebaseUser?) {
+    /**
+     * Update the UI related to user profile
+     */
+    private fun updateProfileUI(user: FirebaseUser?) {
         if (user != null) {
             if (user.displayName != null) {
                 navHeaderNameTextView.text = user.displayName
@@ -212,17 +240,58 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         this@MainActivity.startActivity(myIntent)
     }
 
-    private fun receiveCash(transaction: Transaction){
+    /**
+     * Send a transaction to the server through POST
+     */
+    private fun sendTransaction(transaction: Transaction) {
+        val params = JSONObject()
+        params.put("type", transaction.intention)
+        params.put("title", transaction.title)
+        params.put("description", transaction.description)
+        params.put("amount", transaction.amount)
+        params.put("initiator_id", transaction.initiatorId)
+        params.put("receiver_id", transaction.receiverId)
+        params.put("created", transaction.intention)
 
+        val jsonObjReq = object : JsonObjectRequest(Method.POST, "<url>", params,
+                Response.Listener<JSONObject> { response ->
+                    Log.d(TAG, "/post request OK! Response: $response")
+                    Toast.makeText(this, "/post request OK! Response: $response", Toast.LENGTH_LONG).show()
+                },
+                Response.ErrorListener { error ->
+                    VolleyLog.e(TAG, "/post request fail! Error: ${error.message}")
+                    Toast.makeText(this, "/post request fail! Error: ${error.message}", Toast.LENGTH_LONG).show()
+                }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Content-Type", "application/json")
+                return headers
+            }
+        }
+
+        addToRequestQueue(jsonObjReq)
     }
 
-    private fun sendCash(transaction: Transaction){
+    fun <T> addToRequestQueue(request: Request<T>, tag: String) {
+        request.tag = if (TextUtils.isEmpty(tag)) TAG else tag
+        requestQueue?.add(request)
+    }
 
+    fun <T> addToRequestQueue(request: Request<T>) {
+        request.tag = TAG
+        requestQueue?.add(request)
+    }
+
+    fun cancelPendingRequests(tag: Any) {
+        if (requestQueue != null) {
+            requestQueue!!.cancelAll(tag)
+        }
     }
 
     companion object {
-
-        @get:Contract(pure = true)
+        private val TAG = MainActivity::class.java.simpleName
+        @get:Synchronized
         var mainActivity: MainActivity? = null
             private set
     }
--- app/src/main/java/com/ivantha/mobileatm/model/Transaction.kt
+++ app/src/main/java/com/ivantha/mobileatm/model/Transaction.kt
@@ -6,14 +6,28 @@ import java.io.Serializable
 
 class Transaction(var intention: Intention, var amount: Double) : Serializable {
     var timestamp: DateTime = DateTime()
+    var confirmed: Boolean = false
     var title: String? = null
     var description: String? = null
     var initiatorId: String? = null
+    var initiatorName: String? = null
     var receiverId: String? = null
 
+    /**
+     * Intention of starting the transaction
+     */
     enum class Intention {
         SEND,
         REQUEST
+    }
+
+    public fun getStatement(): String {
+        return title +
+                "\n" + description +
+                "\n" + "Amount : " + amount +
+                "\n" + "Requesting party : " + initiatorId +
+                "\n" + "Request : " + intention.toString() +
+                "\n" + "Time : " + timestamp
     }
 
     override fun toString(): String {
