69b752f9345edbc2a556cefbd1dbe31e60f5f90f
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-06-26 04:25:23 +0530

    Rename AccountFragmentComponents

--- app/src/main/java/com/ivantha/mobileatm/fragment/AccountFragment.kt
+++ app/src/main/java/com/ivantha/mobileatm/fragment/AccountFragment.kt
@@ -6,57 +6,42 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Switch
-import android.widget.TextView
 import android.widget.Toast
 import com.ivantha.mobileatm.R
 import com.ivantha.mobileatm.common.Session
+import kotlinx.android.synthetic.main.fragment_account.*
 
 class AccountFragment : Fragment() {
-    var txt_acc_balance: TextView? = null
-    var txt_acc_unverfiedBalance: TextView? = null
-    var txt_acc_spendingLimit: TextView? = null
-    var swt_acc_spendingLimitEnable: Switch? = null
-    var txt_user_firstName: TextView? = null
-    var txt_user_lastName: TextView? = null
-    var btn_save: Button? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_account, container, false)
+        return view
+    }
 
-        txt_acc_balance = view.findViewById(R.id.txt_acc_balance)
-        txt_acc_unverfiedBalance = view.findViewById(R.id.txt_acc_unverfiedBalance)
-        txt_acc_spendingLimit = view.findViewById(R.id.txt_acc_spendingLimit)
-        swt_acc_spendingLimitEnable = view.findViewById(R.id.swt_acc_spendingLimitEnable)
-        txt_user_firstName = view.findViewById(R.id.txt_user_firstName)
-        txt_user_lastName = view.findViewById(R.id.txt_user_lastName)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         if (Session.currentUser != null) {
-            txt_acc_balance!!.text = Session.currentUser!!.account!!.balance.toString()
-            txt_acc_unverfiedBalance!!.text = Session.currentUser!!.account!!.balance.toString()
-            txt_acc_spendingLimit!!.text = Session.currentUser!!.account!!.spendingLimit.toString()
-            swt_acc_spendingLimitEnable!!.isChecked = Session.currentUser!!.account!!.spendingLimitEnable
-            txt_user_firstName!!.text = Session.currentUser!!.firstName
-            txt_user_lastName!!.text = Session.currentUser!!.lastName
+            fragmentAccountFirstNameEditText.setText(Session.currentUser!!.firstName)
+            fragmentAccountLastNameEditText.setText(Session.currentUser!!.lastName)
+            fragmentAccountAccountBalanceEditText.setText(Session.currentUser!!.account!!.balance.toString())
+            fragmentAccountSpendingLimitEditText.setText(Session.currentUser!!.account!!.spendingLimit.toString())
+            fragmentAccountEnableSpendingLimitSwitch.isChecked = Session.currentUser!!.account!!.spendingLimitEnable
         }
 
-        val btn_save = view.findViewById(R.id.btn_save) as Button
-
-        btn_save.setOnClickListener {
+        fragmentAccountSaveButton.setOnClickListener {
             if (Session.currentUser != null) {
-                Session.currentUser!!.account!!.balance = txt_acc_balance!!.text.toString().toDouble()
-                Session.currentUser!!.account!!.spendingLimit = txt_acc_spendingLimit!!.text.toString().toDouble()
-                Session.currentUser!!.account!!.spendingLimitEnable = swt_acc_spendingLimitEnable!!.isChecked
-                Session.currentUser!!.firstName = txt_user_firstName!!.text.toString()
-                Session.currentUser!!.lastName = txt_user_lastName!!.text.toString()
+                Session.currentUser!!.firstName = fragmentAccountFirstNameEditText.text.toString()
+                Session.currentUser!!.lastName = fragmentAccountLastNameEditText.text.toString()
+                Session.currentUser!!.account!!.balance = fragmentAccountAccountBalanceEditText.text.toString().toDouble()
+                Session.currentUser!!.account!!.spendingLimit = fragmentAccountSpendingLimitEditText.text.toString().toDouble()
+                Session.currentUser!!.account!!.spendingLimitEnable = fragmentAccountEnableSpendingLimitSwitch.isChecked
+
                 Session.updateUser()
                 Toast.makeText(activity, "Updated", Toast.LENGTH_LONG).show()
-
             }
         }
-        return view
     }
 
     companion object {
